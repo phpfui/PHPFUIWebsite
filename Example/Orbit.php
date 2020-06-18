15ecd8433b7618e75988bf75f6cbe58a1984d542
Bruce Wells <brucekwells@gmail.com>
2020-06-17 22:35:32 -0400

    Better Orbit warning

--- Example/Orbit.php
+++ Example/Orbit.php
@@ -9,7 +9,7 @@ class Orbit extends \Example\Page
 		{
 		parent::__construct();
 
-		$this->addBody(new \PHPFUI\Header('Orbit Example'));
+		$this->addBody(new \PHPFUI\Header('Orbit Carousel Example'));
 		$this->addBody('The native Foundation carousel.');
 
 		$slider = new \PHPFUI\Orbit('Favorite Space Pictures');
@@ -18,9 +18,8 @@ class Orbit extends \Example\Page
 		$slider->addImageSlide(new \PHPFUI\Image('/images/orbit/03.jpg', 'Encapsulating'), 'Encapsulating');
 		$slider->addImageSlide(new \PHPFUI\Image('/images/orbit/04.jpg', 'Outta This World'), 'Outta This World');
 		$slider->addHTMLSlide($this->getSlide('This is html'));
-		$slider->addHTMLSlide($this->getSlide('This is a warning', 'warning'));
 		$slider->addHTMLSlide($this->getSlide('This is secondary', 'secondary'));
-		$slider->addHTMLSlide($this->getSlide('Warning Will Robinson', 'alert'));
+		$slider->addHTMLSlide($this->getSlide('Warning Will Robinson', 'warning'));
 		$this->addBody($slider);
 		}
 
