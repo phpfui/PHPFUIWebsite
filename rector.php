ac19ff5f994387ad3f095ca2df26856f45cf43d1
Bruce Wells <brucekwells@gmail.com>
2022-12-21 16:38:28 -0500

    Update for Rector

--- rector.php
+++ rector.php
@@ -8,11 +8,11 @@ use Rector\Set\ValueObject\LevelSetList;
 
 return static function (RectorConfig $rectorConfig): void {
     $rectorConfig->paths([
-//												 __DIR__ . '/../PHPFUI/src',
-												 __DIR__ . '/../InstaDoc/src',
-												 __DIR__ . '/../InstaDoc/tests',
+												 __DIR__ . '/../bicycleclubwebsite/app',
+//												 __DIR__ . '/../InstaDoc/src',
+//												 __DIR__ . '/../InstaDoc/tests',
     ]);
 
     // define sets of rules
-    $rectorConfig->sets([LevelSetList::UP_TO_PHP_80]);
+    $rectorConfig->sets([LevelSetList::UP_TO_PHP_82]);
 };
