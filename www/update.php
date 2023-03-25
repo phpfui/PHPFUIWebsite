f8d9c098b7838ac585d7f51d5571abef1beefe0c
Bruce Wells <brucekwells@gmail.com>
2023-03-25 11:47:25 -0400

    Show all errors on update

--- www/update.php
+++ www/update.php
@@ -2,6 +2,15 @@
 
 include '../common.php';
 
+// Turn on error reporting
+error_reporting(E_ALL);
+
+// Turn on display_errors
+ini_set('display_errors', 1);
+
+// Turn on display_startup_errors
+ini_set('display_startup_errors', 1)
+
 $repo = new \Gitonomy\Git\Repository($_SERVER['DOCUMENT_ROOT'] . '/..');
 $wc = $repo->getWorkingCopy();
 $wc->checkout('master');
