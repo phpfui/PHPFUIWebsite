bca753b0517f9d6f988d950528fd3559d93bc4f1
Bruce Wells <brucekwells@gmail.com>
2023-06-09 21:37:57 -0400

    Better RWGPS URL parsing

--- Example/Model/RWGPS.php
+++ Example/Model/RWGPS.php
@@ -47,7 +47,9 @@ class RWGPS
 
 	public function validate() : string
 		{
-		$json = \file_get_contents($this->url . '.json');
+		$parts = \parse_url($this->url);
+		$parts['path'] .= '.json';
+		$json = \file_get_contents($this->unparse_url($parts));
 
 		if (empty($json))
 			{
@@ -108,4 +110,20 @@ class RWGPS
 		// must want meters
 		return $meters;
 		}
+
+	private function unparse_url(array $parsed_url) : string
+		{
+		$scheme = isset($parsed_url['scheme']) ? $parsed_url['scheme'] . '://' : '';
+		$host = $parsed_url['host'] ?? '';
+		$port = isset($parsed_url['port']) ? ':' . $parsed_url['port'] : '';
+		$user = $parsed_url['user'] ?? '';
+		$pass = isset($parsed_url['pass']) ? ':' . $parsed_url['pass'] : '';
+		$pass = ($user || $pass) ? "{$pass}@" : '';
+		$path = $parsed_url['path'] ?? '';
+		$query = isset($parsed_url['query']) ? '?' . $parsed_url['query'] : '';
+		$fragment = isset($parsed_url['fragment']) ? '#' . $parsed_url['fragment'] : '';
+
+		return "{$scheme}{$user}{$pass}{$host}{$port}{$path}{$query}{$fragment}";
+		}
+
 	}
