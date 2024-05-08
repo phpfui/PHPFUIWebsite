f6abcd61d2d8eff53d4e04564cc2089777ed2087
Bruce Wells <brucekwells@gmail.com>
2024-05-08 01:22:25 -0400

    Composer update on 2024-05-08

--- PHPFUI/Input/SelectEnum.php
+++ PHPFUI/Input/SelectEnum.php
@@ -2,22 +2,24 @@
 
 namespace PHPFUI\Input;
 
-class RadioGroupEnum extends \PHPFUI\Input\RadioGroup implements \Countable
+/**
+ * A wrapper for Select control from an enum
+ */
+class SelectEnum extends \PHPFUI\Input\Select
 	{
 	/**
-	 * Construct a Radio Button Enum Group
-	 *
 	 * @param string $name of the button
 	 * @param string $label optional
 	 * @param $enum initial value from the supplied enum
 	 */
 	public function __construct(string $name, string $label, $enum)	// @phpstan-ignore-line
 		{
-		parent::__construct($name, $label, $enum->value);
+		parent::__construct($name, $label);
 
 		foreach ($enum::cases() as $property)
 			{
-			$this->addButton(\ucwords(\strtolower(\str_replace('_', ' ', $property->name))), $property->value);
+			$optionLabel = \ucwords(\strtolower(\str_replace('_', ' ', $property->name)));
+			$this->addOption($optionLabel, $property->value, $property->value == $enum->value);
 			}
 		}
 	}
