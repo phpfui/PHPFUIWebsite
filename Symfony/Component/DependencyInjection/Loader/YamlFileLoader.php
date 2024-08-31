2430c4ae92330b4ca54ac3d2e591d620cc453235
Bruce Wells <brucekwells@gmail.com>
2024-08-31 01:22:24 -0400

    Composer update on 2024-08-31

--- Symfony/Component/DependencyInjection/Loader/YamlFileLoader.php
+++ Symfony/Component/DependencyInjection/Loader/YamlFileLoader.php
@@ -455,8 +455,9 @@ class YamlFileLoader extends FileLoader
             return $return ? $alias : $this->container->setAlias($id, $alias);
         }
 
+        $changes = [];
         if (null !== $definition) {
-            // no-op
+            $changes = $definition->getChanges();
         } elseif ($this->isLoadingInstanceof) {
             $definition = new ChildDefinition('');
         } elseif (isset($service['parent'])) {
@@ -479,7 +480,7 @@ class YamlFileLoader extends FileLoader
             $definition->setAutoconfigured($defaults['autoconfigure']);
         }
 
-        $definition->setChanges([]);
+        $definition->setChanges($changes);
 
         if (isset($service['class'])) {
             $definition->setClass($service['class']);
@@ -561,7 +562,7 @@ class YamlFileLoader extends FileLoader
                 }
 
                 if (\is_string($k)) {
-                    throw new InvalidArgumentException(sprintf('Invalid method call for service "%s", did you forgot a leading dash before "%s: ..." in "%s"?', $id, $k, $file));
+                    throw new InvalidArgumentException(sprintf('Invalid method call for service "%s", did you forget a leading dash before "%s: ..." in "%s"?', $id, $k, $file));
                 }
 
                 if (isset($call['method']) && \is_string($call['method'])) {
