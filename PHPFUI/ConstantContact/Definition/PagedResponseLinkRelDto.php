22b21243798800329e51bcbda9f6c5de7f133fd2
Bruce Wells <brucekwells@gmail.com>
2026-05-05 01:22:21 -0400

    Composer update on 2026-05-05

---
+++ PHPFUI/ConstantContact/Definition/PagedResponseLinkRelDto.php
@@ -0,0 +1,16 @@
+<?php
+
+// Generated file. Do not edit by hand. Use update.php in project root.
+
+namespace PHPFUI\ConstantContact\Definition;
+
+/**
+ * @property string $href The URI of a page of results.
+ */
+class PagedResponseLinkRelDto extends \PHPFUI\ConstantContact\Definition\Base
+	{
+	protected static array $fields = [
+		'href' => 'string',
+
+	];
+	}
